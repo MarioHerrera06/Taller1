e3c70b8f5b2d50db20d5c44d1b8fee47ca3e7a1a
Administrador <Administrador@SALA-EDI-104-20>
2017-08-25 17:31:26 -0500

    add attributes , setters and getters

--- Cocina/src/java/Data/Repice.java
+++ Cocina/src/java/Data/Repice.java
@@ -5,10 +5,51 @@
  */
 package Data;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Administrador
  */
 public class Repice {
+    private String name;
+    private ArrayList<Ingredient> ingredients;
+    private ArrayList<String> Preparation;
+    private  int cookingTime;
+
+    public Repice(){}
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(ArrayList<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public ArrayList<String> getPreparation() {
+        return Preparation;
+    }
+
+    public void setPreparation(ArrayList<String> Preparation) {
+        this.Preparation = Preparation;
+    }
+
+    public int getCookingTime() {
+        return cookingTime;
+    }
+
+    public void setCookingTime(int cookingTime) {
+        this.cookingTime = cookingTime;
+    }
+    
     
 }
