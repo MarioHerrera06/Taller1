e4dec33c383d3d8f8bfda37599280cb759bc7470
Administrador <Administrador@SALA-EDI-104-20>
2017-08-25 17:35:06 -0500

    add attributes, getter and setter

--- Cocina/src/java/Data/Ingredient.java
+++ Cocina/src/java/Data/Ingredient.java
@@ -11,4 +11,12 @@ package Data;
  */
 public class Ingredient {
     String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
