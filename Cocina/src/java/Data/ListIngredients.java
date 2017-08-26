e29c48cb2b173d68090bb137a54008f75c0e6d89
stiven <stiven@stiven-PC>
2017-08-26 16:51:56 -0500

    i create class listIngredients

---
+++ Cocina/src/java/Data/ListIngredients.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author stiven
+ */
+
+public class ListIngredients {
+    public static ArrayList<Ingredient> listIngrediendts;
+    
+    public ListIngredients()
+        {
+        listIngrediendts=new ArrayList<>();
+        }
+}
